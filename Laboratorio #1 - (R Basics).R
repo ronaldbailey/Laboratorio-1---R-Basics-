333be39f2ed9626039355f623efc2b94d869df34
ronaldbailey <ronald.bailey@galileo.edu>
2023-04-29 15:59:52 -0600

    Primera y segunda pregunta

--- Laboratorio #1 - (R Basics).R
+++ Laboratorio #1 - (R Basics).R
@@ -1,3 +1,35 @@
+
+
+############################################################################################################################################3
 #1. ¿Cómo generaría un arreglo aleatorio de 250 elementos con los colores primarios? 
 
 
+# Crear un vector con los colores primarios
+colores_primarios = c("rojo", "verde", "azul")
+
+# Generar un arreglo aleatorio de 250 elementos utilizando la función sample()
+arreglo_aleatorio = sample(colores_primarios, 250, replace = TRUE)
+
+# Ver el arreglo generado
+print(arreglo_aleatorio)
+
+########################################################################################################################
+
+# 2. Dado un arreglo de 120000 elementos de números enteros entre
+# 40 y 70, ¿Cómo haría para obtener la desviación estándar de
+# aquellos números que son mayores a 55 y menores a 64?
+
+# Generar un arreglo de 120000 elementos de números enteros entre 40 y 70
+
+set.seed(42) # Fijar una semilla para la reproducibilidad (esto tambien lo uso en python)
+
+arreglo <- sample(40:70, 120000, replace = TRUE)
+
+# Filtrar los números que son mayores a 55 y menores a 64
+numeros_filtrados <- arreglo[arreglo > 55 & arreglo < 64]
+
+# Calcular la desviación estándar de los números filtrados
+desviacion_estandar <- sd(numeros_filtrados)
+
+# Mostrar la desviación estándar
+print(desviacion_estandar)
