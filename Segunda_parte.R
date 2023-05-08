8dec1479ad380066ea3c853145247f8a13088a28
ronaldbailey <ronald.bailey@galileo.edu>
2023-05-07 18:30:36 -0600

    Segunda parte redme

--- Segunda_parte.R
+++ Segunda_parte.R
@@ -1,1 +1,25 @@
-1+1
+# Problmea #2:
+
+# Para entregar este ejercicio deberá subir un archivo .R con la
+# pregunta en forma de comentario, el código que generó y su respuesta
+# como comentario. Esta ejercicio requerirá un poco de investigación,
+# no puede utilizar ninguna otra cosa que no se le haya enseñado en
+# clase como librerías, funciones y demás, utilice la menor cantidad
+# de líneas posible (1 línea idealmente), NO puede utilizar ninguna
+# estructura de control de flujo (ciclos y condicionales).
+
+# Antes de realizar este ejercicio limpie el Entorno de variales que
+# ha construido hasta ahora, cargue el archivo data.Rdata en RStudio.
+# Al cargarlo notará que existe una única variable llamada
+# info_estidiantes, para dicha variable responda la siguientes
+# preguntas: 
+
+##########################Carga del archivo 
+load("Lab1/data.RData")
+
+
+#1. ¿Qué tipo de dato es info_estudiantes? 
+typeof(info_estudiantes)
+######################El dato es tipo list
+
+
